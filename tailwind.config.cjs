2a46f63a1e78578f085ac78a208462da6e1ff733
ZinoChan <devzinochan@gmail.com>
2022-12-19 22:45:32 +0100

    add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary1: '#F2304D',
+				light: '#F4F6F8',
+				dark1: '#131217',
+				dark2: '#191B1F',
+				dark3: '#1C1E22',
+				dark4: '#26272E'
+			}
+		}
 	},
 	plugins: []
 };
