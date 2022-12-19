dcca56fe12827a26f0ffc7e2f379280ed8f04ccc
ZinoChan <devzinochan@gmail.com>
2022-12-19 13:32:29 +0100

    add tailwindcss & sass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+import '../styles/app.scss'
+    </script>
+
+    <main>
+        <h1 class="text-4xl text-red-200 font-bold">hello world</h1>
+        <slot/>
+    </main>
+
+    <footer>
+        copy right
+    </footer>
