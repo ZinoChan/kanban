28319bce0d1aaab2a53e6c302a86858c9c4073e1
ZinoChan <devzinochan@gmail.com>
2022-12-20 21:24:04 +0100

    feat: Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,35 @@
+<nav
+	class="fixed z-10 h-16 min-h-16 max-h-16 top-0 left-0 bg-white px-2 sm:px-4 py-2.5 dark:bg-dark1  w-full border-b border-gray-200 dark:border-dark3"
+>
+	<div class="container flex flex-wrap items-center justify-between mx-auto">
+		<a href="/" class="flex items-center">
+			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-primary1">
+				Kanban
+			</span>
+		</a>
+		<div class="flex md:order-2">
+			<button type="button" class="btn-primary">get started</button>
+			<button
+				data-collapse-toggle="navbar-sticky"
+				type="button"
+				class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+				aria-controls="navbar-sticky"
+				aria-expanded="false"
+			>
+				<span class="sr-only">Open main menu</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/></svg
+				>
+			</button>
+		</div>
+	</div>
+</nav>
