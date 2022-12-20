b741fc63939ea7a37c60dac4c99fcee815847427
ZinoChan <devzinochan@gmail.com>
2022-12-20 21:23:11 +0100

    refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-import '../styles/app.scss'
-    </script>
+	import '../styles/app.scss';
+	import Header from '../components/Header.svelte';
+	import SideNav from '../components/SideNav.svelte';
+</script>
 
-    <main>
-        <h1 class="text-4xl text-red-200 font-bold">hello world</h1>
-        <slot/>
-    </main>
+<main class="flex flex-col">
+	<Header />
+	<div class="relative flex w-full top-16">
+		<SideNav />
+		<div
+			class="absolute w-screen min-h-[calc(100vh-64px)] max-h-[calc(100vh-64px)] overflow-auto transition-all sm:translate-x-[256px] lg:translate-x-[300px] max-w-[calc(100vw+256px)] bg-light dark:bg-dark2"
+		>
+			<slot />
+		</div>
+	</div>
+</main>
 
-    <footer>
-        copy right
-    </footer>
+<footer>copy right</footer>
