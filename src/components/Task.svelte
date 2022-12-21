f25acf989a118a2350e3e8cb12a3f2aa903ca8fa
ZinoChan <devzinochan@gmail.com>
2022-12-21 21:31:32 +0100

    feat: task component

---
+++ src/components/Task.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Bolt from '../svgs/Bolt.svelte';
+</script>
+
+<div
+	class="cursor-pointer transition duration-75 flex items-center space-x-4 p-4 border border-gray-200 dark:border-dark3 text-sm group dark:hover:bg-dark3 bg-white dark:bg-dark1 rounded-lg max-w-[280px] w-[280px] hover:shadow-md"
+>
+	<Bolt />
+	<span class="sr-only">important task</span>
+	<div>
+		<h3 class="text-lg font-bold text-gray-900 dark:text-gray-300 group-hover:text-primary1 mb-3">
+			Build Login page
+		</h3>
+		<p class="text-bold text-xs text-gray-600 dark:text-gray-300">0 of 2 subtasks</p>
+	</div>
+</div>
