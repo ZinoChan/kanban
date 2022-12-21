3ea1bc8e7e0878be2bc3db79479e9934bd455b10
ZinoChan <devzinochan@gmail.com>
2022-12-21 21:35:28 +0100

    refactor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,9 +8,7 @@
 	<Header />
 	<div class="relative flex w-full top-16">
 		<SideNav />
-		<div
-			class="absolute w-screen min-h-[calc(100vh-64px)] max-h-[calc(100vh-64px)] overflow-auto transition-all sm:translate-x-[256px] lg:translate-x-[300px] max-w-[calc(100vw+256px)] bg-light dark:bg-dark2"
-		>
+		<div class="absolute w-full bg-light dark:bg-dark2">
 			<slot />
 		</div>
 	</div>
