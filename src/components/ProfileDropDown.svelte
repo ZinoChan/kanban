732033b5fc4525a2e9c5e59c38df020cd1399ed3
ZinoChan <devzinochan@gmail.com>
2022-12-22 18:13:02 +0100

    feat: dropDown

---
+++ src/components/ProfileDropDown.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Trending from '../svgs/Trending.svelte';
+	import User from '../svgs/User.svelte';
+	import MoreVertical from '../svgs/MoreVertical.svelte';
+	let showDropDown = false;
+</script>
+
+<div>
+	<button
+		on:click={() => (showDropDown = !showDropDown)}
+		class="inline-flex items-center p-2 text-sm font-medium text-center group"
+		type="button"
+	>
+		<MoreVertical />
+	</button>
+	<div
+		class={`${
+			showDropDown ? 'flex flex-col' : 'hidden'
+		} absolute top-full right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-dark3 dark:divide-gray-600`}
+	>
+		<ul
+			class="py-1 text-sm text-gray-700 dark:text-gray-200"
+			aria-labelledby="dropdownMenuIconButton"
+		>
+			<li>
+				<a
+					href="#"
+					class="font-bold capitalize flex space-x-2 py-2 px-4 group hover:bg-gray-100 dark:hover:bg-dark4 dark:hover:text-white"
+				>
+					<User />
+					<span class="inline-flex self-end">profile</span>
+				</a>
+			</li>
+			<li>
+				<a
+					href="#"
+					class="font-bold capitalize flex  space-x-2 py-2 px-4 group hover:bg-gray-100 dark:hover:bg-dark4 dark:hover:text-white"
+				>
+					<Trending />
+					<span class="inline-flex self-end"> progress </span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
