e0364fbc7324b73fd1a8693c417a174299723f91
ZinoChan <devzinochan@gmail.com>
2022-12-22 18:15:36 +0100

    svgs

---
+++ src/svgs/MoreVertical.svelte
@@ -0,0 +1,15 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="flex-shrink-0 w-4 h-4 text-gray-500 transition duration-75 group-hover:text-primary1 dark:text-gray-400 "
+	><circle cx="12" cy="12" r="1" /><circle cx="12" cy="5" r="1" /><circle
+		cx="12"
+		cy="19"
+		r="1"
+	/></svg
+>
