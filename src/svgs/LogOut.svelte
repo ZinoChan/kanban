a13f2132362a50c353bcb51102dc79f421460710
ZinoChan <devzinochan@gmail.com>
2022-12-23 19:19:46 +0100

    svgs

--- src/svgs/LogOut.svelte
+++ src/svgs/LogOut.svelte
@@ -7,10 +7,10 @@
 	stroke-linecap="round"
 	stroke-linejoin="round"
 	class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-primary1"
-	><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /><polyline points="10 17 15 12 10 7" /><line
-		x1="15"
+	><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line
+		x1="21"
 		y1="12"
-		x2="3"
+		x2="9"
 		y2="12"
 	/></svg
 >
