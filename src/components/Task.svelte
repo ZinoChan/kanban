40b40aa6ba600be6a6ec958bcab05e77bcd29a23
ZinoChan <devzinochan@gmail.com>
2022-12-27 22:07:23 +0100

    refactor & add data

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,19 +1,32 @@
-<script>
-	import Bolt from '../svgs/Bolt.svelte';
-	import { showSubTasksModal } from '../stores/settings';
+<script lang="ts">
+	import type { ITask } from '../types/Board.types';
+	import { currentTask, showSubTasksModal } from '../stores/settings';
+	import PriorityIcon from './PriorityIcon.svelte';
+	export let taskData: ITask;
+	let completedCount = taskData
+		? taskData.subTasks.filter((subTask) => subTask.isCompleted).length
+		: 0;
+	function onTaskclick() {
+		currentTask.set(taskData);
+		showSubTasksModal.update((val) => (val = !val));
+	}
 </script>
 
 <div
-	on:click={() => showSubTasksModal.update((val) => (val = !val))}
+	on:click={onTaskclick}
 	on:keyup={null}
 	class="cursor-pointer transition duration-75 flex items-center space-x-4 p-4 border border-gray-200 dark:border-dark3 text-sm group dark:hover:bg-dark3 bg-white dark:bg-dark1 rounded-lg max-w-[280px] w-[280px] hover:shadow-md"
 >
-	<Bolt />
-	<span class="sr-only">important task</span>
+	<PriorityIcon priority={taskData.taskPriority} />
+	<span class="sr-only">
+		{taskData.taskPriority}
+	</span>
 	<div>
 		<h3 class=" font-bold text-gray-900 dark:text-gray-200 group-hover:text-primary1 mb-3">
-			Build Login page
+			{taskData.taskTitle}
 		</h3>
-		<p class="text-bold text-sm text-gray-600 dark:text-gray-300">0 of 2 subtasks</p>
+		<p class="text-bold text-sm text-gray-600 dark:text-gray-300">
+			{completedCount} of {taskData.subTasks.length} subtasks
+		</p>
 	</div>
 </div>
