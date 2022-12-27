6ec8813fdd974594dd0945570051423a09da5719
ZinoChan <devzinochan@gmail.com>
2022-12-27 22:02:38 +0100

    add data to localStorage

---
+++ src/stores/data.ts
@@ -0,0 +1,19 @@
+// src/stores/content.js
+import { dummyData } from '../data';
+import { writable } from 'svelte/store';
+import type { TData } from 'src/types/Data.types';
+
+const storageData = window?.localStorage?.getItem('data');
+const defaultValue = dummyData;
+const isBrowser = typeof window !== 'undefined';
+
+const initialValue =
+	isBrowser && storageData ? JSON.parse(storageData) ?? defaultValue : defaultValue;
+
+export const data = writable<TData>(initialValue);
+
+data.subscribe((value) => {
+	if (isBrowser) {
+		window.localStorage.setItem('data', JSON.stringify(value));
+	}
+});
