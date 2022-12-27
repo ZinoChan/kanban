97a70a942741fecfd6fe6e4a0cd70781245e6168
ZinoChan <devzinochan@gmail.com>
2022-12-27 22:03:02 +0100

    dynamic route

---
+++ src/routes/boards/[slug]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { TBoard } from '../../../types/Board.types';
+	import { isSideBarOpen } from '../../../stores/settings';
+	import BoardColumn from '../../../components/BoardColumn.svelte';
+	export let data: TBoard;
+</script>
+
+<section
+	class={`main-container ${
+		$isSideBarOpen ? 'sm:translate-x-[256px] lg:translate-x-[300px]' : 'translate-x-0'
+	}`}
+>
+	{#if data}
+		<div class="flex items-center space-x-6">
+			{#each data.columns as column (column.id)}
+				<BoardColumn columnData={column} />
+			{/each}
+		</div>
+	{:else}
+		<div class="text-gray-500">loading data...</div>
+	{/if}
+</section>
