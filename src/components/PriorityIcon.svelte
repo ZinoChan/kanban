1e69e8fdb4079bf285a9a3218f6ca45c3035bb01
ZinoChan <devzinochan@gmail.com>
2022-12-27 22:06:34 +0100

    add priorityIcons

---
+++ src/components/PriorityIcon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Alert from '../svgs/Alert.svelte';
+	import Bolt from '../svgs/Bolt.svelte';
+	import Feather from '../svgs/Feather.svelte';
+	import Target from '../svgs/Target.svelte';
+
+	export let priority: String;
+</script>
+
+{#if priority === 'important'}
+	<Alert />
+{:else if priority === 'urgent'}
+	<Bolt />
+{:else if priority === 'later'}
+	<Feather />
+{:else}
+	<Target />
+{/if}
