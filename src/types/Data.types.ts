06533e62aed20fd480585eb1303e0c3362d3f9f0
ZinoChan <devzinochan@gmail.com>
2022-12-28 12:55:03 +0100

    add types

--- src/types/Data.types.ts
+++ src/types/Data.types.ts
@@ -2,5 +2,12 @@ import type { TBoard } from './Board.types';
 
 export type TData = {
 	boards: TBoard[];
-	ideas: string[];
+	ideas: TIdea[];
+};
+
+export type TIdea = {
+	name: string;
+	tags: string[];
+	description: string;
+	id: string;
 };
