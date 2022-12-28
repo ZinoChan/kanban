141bb81f97f0fd6fa1c388fdda60c007953562a2
ZinoChan <devzinochan@gmail.com>
2022-12-28 12:52:42 +0100

    refactor & add data

--- src/components/TaskDetails.svelte
+++ src/components/TaskDetails.svelte
@@ -3,13 +3,27 @@
 	import { showSubTasksModal } from '../stores/settings';
 	import Close from '../svgs/Close.svelte';
 	import DeleteTask from './DeleteTask.svelte';
+	import { currentTask } from '../stores/settings';
+	import type { ITask } from '../types/Board.types';
+	import { data } from '../stores/data';
+
+	$: task = $data.boards
+		.find((board) => board.slug === $currentTask.boardSlug)
+		?.columns.find((col) => col.colName === $currentTask.status)
+		?.colTasks.find((t) => t.id === $currentTask.id);
 	let showDropDown = false;
-	let subtasks = [{ id: 2, value: 'design header', checked: false }];
 	let showDelModal = false;
+
+	$: completedCount = task ? task.subTasks.filter((subTask) => subTask.isCompleted).length : 0;
+
+	function onCloseModal() {
+		currentTask.set({} as ITask);
+		showSubTasksModal.set(false);
+	}
 </script>
 
 <div
-	on:click|self={() => showSubTasksModal.set(false)}
+	on:click|self={onCloseModal}
 	on:keyup={null}
 	class={`${
 		$showSubTasksModal ? '' : 'hidden'
@@ -18,7 +32,6 @@
 	<div
 		class="modal-box relative w-full h-full max-w-md md:h-auto max-h-[calc(100vh-64px)] overflow-y-auto"
 	>
-		<!-- Modal content -->
 		<div class="relative bg-white rounded-lg shadow dark:bg-dark2">
 			<div class="absolute flex items-center top-3 right-2.5 space-x-3">
 				<button
@@ -30,7 +43,7 @@
 				</button>
 				<button
 					type="button"
-					on:click={() => showSubTasksModal.set(false)}
+					on:click={onCloseModal}
 					class="border border-gray-300 dark:border-dark3  text-gray-400 bg-transparent hover:bg-light hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
 				>
 					<Close />
@@ -63,37 +76,39 @@
 
 			<div class="px-6 py-6 lg:px-8">
 				<h3 class="mb-4 capitalize text-xl font-bold text-gray-900 dark:text-white">
-					Design Home Page
+					{task?.taskTitle}
 				</h3>
 				<form class="space-y-6" action="#">
 					<div>
-						<span class="block mb-4 font-bold text-gray-900 dark:text-gray-200">
-							Subtasks (1 of 1)
-						</span>
+						{#if task && task.subTasks?.length > 0}
+							<span class="block mb-4 font-bold text-gray-900 dark:text-gray-200">
+								{completedCount} of {task?.subTasks.length || 0} subtasks
+							</span>
 
-						{#each subtasks as task}
-							<div
-								on:click={() => (task.checked = !task.checked)}
-								on:keyup={() => (task.checked = !task.checked)}
-								class={`${
-									task.checked && 'line-through text-gray-400'
-								} flex items-center py-2 pl-4 rounded border mb-2 border-gray-200 hover:bg-gray-200 dark:hover:bg-dark3 cursor-pointer dark:border-dark3 bg-light dark:bg-dark1`}
-							>
-								<input
-									type="checkbox"
-									checked={task.checked}
-									class="w-4 h-4 mr-2 text-primary3 bg-gray-100 rounded border-gray-300 focus:ring-primary3 dark:focus:primary3 dark:ring-offset-dark3 focus:ring-1 dark:bg-dark2 dark:border-dark3"
-								/>
-								<label
-									for={task.value}
+							{#each task.subTasks as { title, isCompleted, id } (id)}
+								<div
+									on:click={() => (isCompleted = !isCompleted)}
+									on:keyup={() => (isCompleted = !isCompleted)}
 									class={`${
-										task.checked ? 'done' : ''
-									} contents ml-2 cursor-pointer w-full text-sm font-bold capitalize text-gray-900 dark:text-gray-300`}
+										isCompleted && 'line-through text-gray-400'
+									} flex items-center py-2 pl-4 rounded border mb-2 border-gray-200 hover:bg-gray-200 dark:hover:bg-dark3 cursor-pointer dark:border-dark3 bg-light dark:bg-dark1`}
 								>
-									{task.value}
-								</label>
-							</div>
-						{/each}
+									<input
+										type="checkbox"
+										checked={isCompleted}
+										class="w-4 h-4 mr-2 text-primary3 bg-gray-100 rounded border-gray-300 focus:ring-primary3 dark:focus:primary3 dark:ring-offset-dark3 focus:ring-1 dark:bg-dark2 dark:border-dark3"
+									/>
+									<label
+										for={title}
+										class={`${
+											isCompleted ? 'done' : ''
+										} contents ml-2 cursor-pointer w-full text-sm font-bold capitalize text-gray-900 dark:text-gray-300`}
+									>
+										{title}
+									</label>
+								</div>
+							{/each}
+						{/if}
 						<div class="mt-4">
 							<label
 								for="status"
