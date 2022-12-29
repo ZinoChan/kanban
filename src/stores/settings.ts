8420746c614744032f6d6803e6bca5f5bc916e1e
ZinoChan <devzinochan@gmail.com>
2022-12-29 11:48:42 +0100

    clean up

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -1,10 +1,12 @@
-import type { BoardColumn } from '../types/BoardColumn.types';
+import type { ITask } from 'src/types/Board.types';
 import { writable } from 'svelte/store';
 
-export const boardColumns = writable<BoardColumn[]>([]);
+export const boardColumns = writable<any>([]);
 
-export const subtaskColumns = writable<BoardColumn[]>([]);
+export const subtaskColumns = writable<any>([]);
 
 export const showSubTasksModal = writable<boolean>(false);
 
 export const isSideBarOpen = writable<boolean>(true);
+
+export const currentTask = writable<ITask>({} as ITask);
