a5195ad74c7fb896536f86f2439a51b045574c5f
ZinoChan <devzinochan@gmail.com>
2022-12-30 22:58:15 +0100

    clean up

--- src/routes/boards/[slug]/+page.ts
+++ src/routes/boards/[slug]/+page.ts
@@ -1,19 +1,18 @@
 import { error } from '@sveltejs/kit';
 import { data } from '../../../stores/data';
-import type { TData } from 'src/types/Data.types';
+import { get } from 'svelte/store';
+import { currentBoardId } from '../../../stores/selectedBoard';
 
 export const ssr = false;
 
-export function load({ params }: { params: any }) {
-	let $data: TData | undefined;
-
-	data.subscribe((value) => {
-		$data = value;
-	});
+export function load({ params }: { params: { slug: string } }) {
+	const $data = get(data);
 
 	const boardData = $data && Object.values($data.boards).find((board) => board.slug == params.slug);
 
-	if (boardData) return { ...boardData };
-
+	if (boardData) {
+		currentBoardId.set(boardData.id);
+		return { ...boardData };
+	}
 	throw error(404, 'Not Found');
 }
