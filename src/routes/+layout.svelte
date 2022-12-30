e14fcce6cc60650dda336ecb0dfbd80f0d3dbd04
ZinoChan <devzinochan@gmail.com>
2022-12-30 22:57:52 +0100

    minor change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,17 @@
-<script>
+<script lang="ts">
 	import '@fontsource/montserrat';
 	import '@fontsource/hind';
 	import '../styles/app.scss';
 	import Header from '../components/Header.svelte';
 	import SideNav from '../components/SideNav.svelte';
 	import TaskDetails from '../components/TaskDetails.svelte';
+	export let data: { isBoardPage: boolean };
 </script>
 
 <main class="flex flex-col">
 	<Header />
 	<div class="relative flex w-full top-16">
-		<SideNav />
+		<SideNav isBoardPage={data.isBoardPage} />
 		<div class="absolute w-full bg-light dark:bg-dark2">
 			<slot />
 		</div>
