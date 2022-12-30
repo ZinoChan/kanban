35500ef6392ba4b53118f444efee144f695fa12e
ZinoChan <devzinochan@gmail.com>
2022-12-30 22:53:51 +0100

    list board cols

---
+++ src/components/BoardColumnsList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { data } from '../stores/data';
+	import type { TBoard } from '../types/Board.types';
+	import BoardColumn from './BoardColumn.svelte';
+
+	export let boardData: TBoard;
+
+	$: _colList = Object.values($data.boards[boardData.id].columns);
+</script>
+
+<div class="flex  space-x-6">
+	{#each _colList as column (column.id)}
+		<BoardColumn columnData={column} />
+	{/each}
+</div>
