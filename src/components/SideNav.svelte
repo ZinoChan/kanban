43406cbb898b12071b2c40b0c901a9fe14ca8e24
ZinoChan <devzinochan@gmail.com>
2023-01-02 22:01:10 +0100

    fix typo

--- src/components/SideNav.svelte
+++ src/components/SideNav.svelte
@@ -107,7 +107,7 @@
 
 				<li>
 					<a
-						href="/idea"
+						href="/ideas"
 						class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white group hover:bg-gray-100 dark:hover:bg-dark2"
 					>
 						<Flame />
