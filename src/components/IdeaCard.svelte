addec0240585fc8236010bd65585c9e0b85b71fb
ZinoChan <devzinochan@gmail.com>
2023-01-03 21:05:52 +0100

    refactor

--- src/components/IdeaCard.svelte
+++ src/components/IdeaCard.svelte
@@ -1,10 +1,16 @@
+<script lang="ts">
+	import { currentIdea } from '../stores/selectedIdea';
 	import type { TIdea } from '../types/Data.types';
 	import { showIdeaDetails } from '../stores/settings';
 	export let idea: TIdea;
+	function onIdeaClick() {
+		currentIdea.set(idea);
+		showIdeaDetails.set(true);
+	}
 </script>
 
 <div
-	on:click={() => showIdeaDetails.set(true)}
+	on:click={onIdeaClick}
 	on:keyup={null}
 	class="cursor-pointer transition duration-75 flex items-center  p-4 border border-gray-200 dark:border-dark3 text-sm group dark:hover:bg-dark3 bg-white dark:bg-dark1 rounded-lg  hover:shadow-md"
 >
