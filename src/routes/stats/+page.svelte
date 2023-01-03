b6d5c728eef9f1e98c0bb4b97acbfa13bccbd45f
ZinoChan <devzinochan@gmail.com>
2023-01-03 21:07:22 +0100

    add progress page

---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,40 @@
+<section
+	class="transition-all sm:translate-x-[256px] lg:translate-x-[300px] lg:max-w-[calc(100vw-300px)] sm:max-w-[calc(100vw-256px)] min-h-[calc(100vh-64px)] max-h-[calc(100vh-64px)]"
+>
+	<div class="max-w-full mx-4 py-6 sm:mx-auto sm:px-6 lg:px-8">
+		<div class="flex flex-col lg:max-x-md justify-center items-center sm:space-x-4">
+			<div
+				class="inline-block align-bottom bg-white dark:bg-dark1 rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8"
+			>
+				<div class=" p-5">
+					<div class="sm:flex justify-between p-4 sm:items-center">
+						<h3 class="text-sm leading-6 font-medium text-gray-400">Total Tasks</h3>
+						<p class="text-3xl font-bold text-black dark:text-white">75</p>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="inline-block align-bottom bg-white dark:bg-dark1 rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8"
+			>
+				<div class=" p-5">
+					<div class="sm:flex justify-between p-4 sm:items-start">
+						<h3 class="text-sm leading-6 font-medium text-gray-400">Total Tasks today</h3>
+						<p class="text-3xl font-bold text-black dark:text-white">75</p>
+					</div>
+				</div>
+			</div>
+
+			<div
+				class="inline-block align-bottom bg-white dark:bg-dark1 rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8"
+			>
+				<div class=" p-5">
+					<div class="sm:flex justify-between p-4 sm:items-start">
+						<h3 class="text-sm leading-6 font-medium text-gray-400">completed Tasks</h3>
+						<p class="text-3xl font-bold text-black dark:text-white">75</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
