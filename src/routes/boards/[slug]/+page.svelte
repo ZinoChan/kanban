8fc59c28965a163c4738c4455b252af53c448598
ZinoChan <devzinochan@gmail.com>
2023-01-04 18:45:29 +0100

    crud components

--- src/routes/boards/[slug]/+page.svelte
+++ src/routes/boards/[slug]/+page.svelte
@@ -11,6 +11,24 @@
 		$isSideBarOpen ? 'sm:translate-x-[256px] lg:translate-x-[300px]' : 'translate-x-0'
 	}`}
 >
+	<div class="flex space-x-4 items-center mb-8">
+		<h1 class="font-bold text-gray-900 dark:text-gray-100 text-2xl capitalize">
+			{data.name}
+		</h1>
+		<button on:click={onDeleteBoard} class="group btn-theme  p-1">
+			<Trash />
+		</button>
+
+		<button
+			on:click={onEditBoard}
+			class="edit-btn btn-theme  text-gray-500 transition duration-7 dark:text-gray-400 p-1"
+		>
+			<Edit />
+		</button>
+		<button on:click={onAddCol} class="group btn-theme p-1">
+			<Plus />
+		</button>
+	</div>
 	{#if data}
 		<div class="flex  space-x-6">
 			<BoardColumnsList boardData={data} />
@@ -19,3 +37,9 @@
 		<div class="text-gray-500">loading data...</div>
 	{/if}
 </section>
+
+<style lang="scss">
+	.edit-btn:hover {
+		color: rgb(8 145 178) !important;
+	}
+</style>
