24a6eccf878dcb93a6e62789c73356595cdb4114
ZinoChan <devzinochan@gmail.com>
2023-01-06 17:32:57 +0100

    edit existing cols

--- src/components/EditBoard.svelte
+++ src/components/EditBoard.svelte
@@ -5,6 +5,10 @@
 	import { data } from '../stores/data';
 
 	$: currentBoard = $data.boards[$currentBoardId];
+	$: boardCols = currentBoard?.columns && Object.values(currentBoard?.columns);
+	function removeColumn(id: string) {
+		boardCols = boardCols.filter((col) => id !== col.id);
+	}
 </script>
 
 <div
@@ -46,6 +50,32 @@
 							class="block mb-2 text-sm font-bold text-gray-900 dark:text-gray-200"
 							>Board Columns</label
 						>
+						{#if boardCols}
+							{#each boardCols as column}
+								<div class="flex mb-3 items-center space-x-2">
+									<div class="flex items-center space-x-1">
+										<input
+											bind:value={column.colName}
+											type="text"
+											class="form-input"
+											placeholder="Your title.."
+										/>
+										<div>
+											<ColorPicker bind:value={column.colColor} />
+										</div>
+									</div>
+
+									<button
+										on:click={() => removeColumn(column.id)}
+										type="button"
+										class="border border-gray-300 dark:border-dark3  text-gray-400 bg-transparent hover:bg-light hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+									>
+										<Close />
+										<span class="sr-only">remove column</span>
+									</button>
+								</div>
+							{/each}
+						{/if}
 					</div>
 					<button type="button" class="btn-secondary w-full">add Column</button>
 					<button type="submit" class="btn-primary w-full">Create Board</button>
