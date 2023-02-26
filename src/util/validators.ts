a10ed19890392ac9aa5b56abc2df09fa4ac6b216
ZinoChan <devzinochan@gmail.com>
2023-02-26 15:25:32 +0100

    refactor

--- src/util/validators.ts
+++ src/util/validators.ts
@@ -1,13 +1,11 @@
-function emailValidator() {
-	return function email(value: string) {
+function emailValidator(value: string) {
 		return (
 			(value &&
 				!!value.match(
 					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 				)) ||
-			'Please enter a valid email'
+		false
 		);
-	};
 }
 
 function requiredValidator() {
