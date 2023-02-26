65a517178e9d2b35c8c862508aaf52f82e3f3e43
ZinoChan <devzinochan@gmail.com>
2023-02-26 15:26:44 +0100

    add passwordValidator

--- src/util/validators.ts
+++ src/util/validators.ts
@@ -1,15 +1,25 @@
 function emailValidator(value: string) {
-		return (
-			(value &&
-				!!value.match(
-					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-				)) ||
+	return (
+		(value &&
+			!!value.match(
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+			)) ||
 		false
-		);
+	);
 }
 
 function fullNameValidator(value: string | null | undefined) {
 	return (value !== undefined && value !== null && value !== '' && value.length > 3) || false;
 }
 
-export { emailValidator, requiredValidator };
+function passwordValidator(password: string): boolean {
+	const minPasswordLength = 8; // Minimum password length
+	const hasUppercase = /[A-Z]/.test(password); // Password contains uppercase letter(s)
+	const hasLowercase = /[a-z]/.test(password); // Password contains lowercase letter(s)
+	const hasNumber = /\d/.test(password); // Password contains number(s)
+
+	// Check if the password meets all the validation rules
+	return password.length >= minPasswordLength && hasUppercase && hasLowercase && hasNumber;
+}
+
+export { emailValidator, fullNameValidator, passwordValidator };
